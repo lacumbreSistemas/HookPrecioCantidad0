0fd6a74a5b442e91f3afcd94db8a6baf9513a209
Gersonn Velasquez <yersonn.velasquez@gmail.com>
2021-10-27 16:56:52 -0600

    inicial

---
+++ Main.vb
@@ -0,0 +1,32 @@
+﻿<ComClass(Main.ClassId, Main.InterfaceId, Main.EventsId)> _
+Public Class Main
+
+#Region "GUID de COM"
+    ' Estos GUID proporcionan la identidad de COM para esta clase 
+    ' y las interfaces de COM. Si las cambia, los clientes 
+    ' existentes no podrán obtener acceso a la clase.
+    Public Const ClassId As String = "16af76d7-a811-4c7c-bb10-817ed8608092"
+    Public Const InterfaceId As String = "cccd6953-88db-40fd-9d57-1d5f7c3c0c72"
+    Public Const EventsId As String = "b509e06d-c110-4413-9119-7486418854ad"
+#End Region
+
+    ' Una clase COM que se puede crear debe tener Public Sub New() 
+    ' sin parámetros, si no la clase no se 
+    ' registrará en el registro COM y no se podrá crear a 
+    ' través de CreateObject.
+    Public Sub New()
+        MyBase.New()
+    End Sub
+
+    Public Function Process(ByVal currSession As Object) As Boolean
+        If currSession.Transaction.TransactionType = 1 And currSession.Transaction.ReturningItems = False Then
+            Dim f As New MetodosVerificacion
+            f.Session = currSession
+            f.RutinaVerificacion()
+            Return f.Correcto
+        End If
+        Return True
+    End Function
+End Class
+
+
---
+++ MetodosVerificacion.vb
@@ -0,0 +1,24 @@
+﻿Imports QSRules
+Public Class MetodosVerificacion
+    Public Session As QSRules.SessionClass
+    Public Correcto As Boolean = True
+
+    Public Sub RutinaVerificacion()
+        For index As Integer = 1 To Session.Transaction.Entries.Count
+            Try
+                If Session.Transaction.Entries(index).Price <= 0 Then
+                    MsgBox("El producto '" + Session.Transaction.Entries(index).Item.Description + "' tiene precio en 0 o menor y no se puede vender, Llamar a un supervisor.")
+                    Correcto = False
+                ElseIf Session.Transaction.Entries(index).Quantity <= 0 Then
+                    MsgBox("El producto  '" + Session.Transaction.Entries(index).Item.Description + "'  tiene Cantidad en 0 o menor y no se puede vender, Llamar a un supervisor.")
+                    Correcto = False
+                ElseIf Session.Transaction.Entries(index).Item.Inactive = True Then
+                    MsgBox("El producto  '" + Session.Transaction.Entries(index).Item.Description + "'  esta Inactivo y no se puede vender, Llamar a un supervisor.")
+                    Correcto = False
+                End If
+            Catch ex As Exception
+                Correcto = False
+            End Try
+        Next
+    End Sub
+End Class
